ebf33338b54006b765a2b7d14a5886d385119b9f
owanemi <owanemi1741@gmail.com>
2024-09-06 23:11:40 +0100

    created deployment script

---
+++ script/DeployOwanemiToken.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {Script} from "lib/forge-std/src/Script.sol";
+import {OwanemiToken} from "src/OwanemiToken.sol";
+
+contract DeployOwanemiToken is Script {
+
+    uint256 public constant INITIAL_SUPPLY = 1e6 ether;
+
+    function run() external {
+        vm.startBroadcast();
+        new OwanemiToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
---
+++ src/OwanemiToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract OwanemiToken is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("OWANEMI-TOKEN", "NEMI"){
+        _mint(msg.sender, _initialSupply);
+    }
+}
